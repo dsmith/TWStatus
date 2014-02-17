35128df1815433a2a91568379fcb7ee22ea1c0b2
Derek Smith <derek.rians.smith@gmail.com>
2014-02-17 16:07:54 -0500

    Only apply the animation if the status text hasn't changed

--- TWStatus/TWStatus.m
+++ TWStatus/TWStatus.m
@@ -11,7 +11,7 @@
     UIWindow *_statusWindow;
     UIView *_backgroundView;
     UILabel *_statusLabel;
-    
+
     UIActivityIndicatorView *_activityIndicator;
 }
 
@@ -34,26 +34,26 @@
     _statusWindow.windowLevel = UIWindowLevelStatusBar;
     _statusWindow.backgroundColor = [UIColor blackColor];
     _statusWindow.alpha = 0.0;
-    
+
     _backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 20)];
     _backgroundView.backgroundColor = [UIColor clearColor];
     _backgroundView.alpha = 0.0;
-    
+
     _statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 20)];
     _statusLabel.backgroundColor = [UIColor clearColor];
     _statusLabel.textColor = [UIColor colorWithRed:191.0/255.0 green:191.0/255.0 blue:191.0/255.0 alpha:1.0];
     _statusLabel.font = [UIFont boldSystemFontOfSize:13];
     _statusLabel.textAlignment = NSTextAlignmentCenter;
     _statusLabel.numberOfLines = 1;
-    
+
     _activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:(UIActivityIndicatorViewStyleWhite)];
     _activityIndicator.transform = CGAffineTransformMakeScale(0.6, 0.6);
     _activityIndicator.frame = CGRectMake(0, 0, 20, 20);
     _activityIndicator.hidesWhenStopped = YES;
-    
+
     [_backgroundView addSubview:_statusLabel];
     [_backgroundView addSubview:_activityIndicator];
-    
+
     [_statusWindow addSubview:_backgroundView];
 }
 
@@ -64,13 +64,13 @@
 }
 
 + (id)sharedTWStatus{
-    
+
     static TWStatus *_sharedTWStatus = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         _sharedTWStatus = [[TWStatus alloc] init];
     });
-    
+
     return _sharedTWStatus;
 }
 
@@ -155,8 +155,7 @@
             
         }];
         
-    }else{
-        
+    }else if(![status isEqualToString:_status]){
         [UIView animateWithDuration:0.1 animations:^{
             
             _backgroundView.alpha = 0.0;
